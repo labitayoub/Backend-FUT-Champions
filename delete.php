1073714101114e4292433faa9af356b37b143d06
Ayoub Labit <ayoub.labite@gmail.com>
2024-12-21 14:52:09 +0100

    Update

--- delete.php
+++ delete.php
@@ -2,16 +2,13 @@
 include('./connection.php');
 
 if (isset($_GET['id'])) {
-    $id = intval($_GET['id']); // Convertir l'ID en entier pour sécurité
-
-    // Exécuter directement la requête de suppression
+    $id = intval($_GET['id']);
+    
     $query = "DELETE FROM players WHERE id = $id";
     if (mysqli_query($conn, $query)) {
         header('Location: player.php?delete_msg=Record deleted successfully');
     } else {
         die("Error deleting record: " . mysqli_error($conn));
     }
-} else {
-    header('Location: player.php?delete_msg=No ID provided for deletion');
-}
+} 
 ?>
